b5feb6433e377020782af2e789eef5ed50d3f426
asifnalband <37806827+asifnalband@users.noreply.github.com>
2022-01-19 17:44:01 +0530

    Add files via upload

---
+++ TestUser.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
+
+namespace BidoneTest
+{
+    public partial class TestUser : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        protected void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TUser ob = new TUser();
+                ob.FirstName = TxtFirstName.Text;
+                ob.LastName = TxtLastName.Text;
+
+                string json = new JavaScriptSerializer().Serialize(ob);
+                System.IO.File.WriteAllText(Server.MapPath("~/testjsonfile.json"), json);
+
+                lblresult.Text = "Data Saved in Json File";
+                Clear();
+
+
+            }
+            catch(Exception er)
+            {
+
+            }
+        }
+
+        private void Clear()
+        {
+            TxtFirstName.Text = string.Empty;
+            TxtLastName.Text = string.Empty;
+        }
+    }
+}
